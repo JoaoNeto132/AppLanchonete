11b53b368360ac5e2968d49ef6690f96aa5bf27b
JoaoNeto132 <joaomasp12@gmail.com>
2023-05-03 14:56:40 -0300

    App criado

---
+++ AppLanchonete/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using AppLanchonete.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppLanchonete.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
